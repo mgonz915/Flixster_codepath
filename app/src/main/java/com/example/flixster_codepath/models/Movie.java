f6b1517c2838a9cb5fc9dd639dc3e75e2d512804
Michael Gonzalez <mgonz915@gmail.com>
2021-11-30 21:22:09 -0700

    Flixster assignment part 2

--- app/src/main/java/com/example/flixster_codepath/models/Movie.java
+++ app/src/main/java/com/example/flixster_codepath/models/Movie.java
@@ -5,22 +5,32 @@ import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
+    int movieId;
     String backdropPath;
     String posterpath;
     String title;
     String overview;
+    double rating;
+
+    // empty constructor needed by the Parceler library
+    public Movie() {
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterpath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJSONArray(JSONArray movieJsonArray) throws JSONException{
@@ -45,5 +55,11 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() { return rating;    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
